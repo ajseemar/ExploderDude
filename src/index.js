382b9b87a5a345a87cb91d23cc61f2bd5cfdd268
Cameron Carter <cameroncarter@utexas.edu>
2019-06-10 14:03:11 -0700

    heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 import openSocket from "socket.io-client";
 
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
 
 // ---------------------------------------------------->
 //                     Client
