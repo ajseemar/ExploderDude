da92685f82ae29b5120b0277914e5943fb6c0d79
Cameron Carter <cameroncarter@utexas.edu>
2019-06-10 15:46:06 -0700

    add /

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("public"));
     
     app.get("/", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "index.html"));
+        res.sendFile(path.resolve(__dirname, "/index.html"));
     });
 } else {
     app.get("/", (req, res) => res.sendFile(__dirname + '/public/index.html'));
