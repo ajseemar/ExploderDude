76823c0573816f9af7d683793444552c94fc0f1d
Cameron Carter <cameroncarter@utexas.edu>
2019-06-18 21:24:56 -0700

    prepare for heroku

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export const url =
     process.env.NODE_ENV === "development" ? development : production;
 
 // export const url = production;
-export const socket = io(development);
+export const socket = io(production);
 //
 // ---------------------------------------------------->
 
